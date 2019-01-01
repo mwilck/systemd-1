openSUSE/systemd

udev: Add migration code for /run/udev/links
If udevd is restarted after applying the changes for /run/udev/links
from "udev: change structure in /run/udev/links to improve efficiency",
it may find the old data structures left over from a previous version. Deal
with this situation by detecting it and moving the target files to the new
places.

src/udev/udev-node.c
@@ -137,9 +137,43 @@ static int make_prio_name(int prio, char *buf, size_t buflen)
         return snprintf(buf, buflen, "%s%d", prio_prefix, prio);
 }
 
+static bool is_prio_name(const char *name, int *priority)
+{
+        int len = sizeof(prio_prefix) - 1;
+        long prio;
+        char *e;
+
+        if (name == NULL ||
+            strncmp(name, prio_prefix, len) || name[len] == '\0')
+                return false;
+
+        prio = strtol(name + len, &e, 10);
+        if (*e != '\0' || prio < INT_MIN || prio >INT_MAX)
+                return false;
+
+        *priority = prio;
+        return true;
+}
+
+static bool is_prio_dirent(DIR *dir, struct dirent *de, int *priority)
+{
+        int prio;
+
+        if (!is_prio_name(de->d_name, &prio))
+                return  false;
+
+        dirent_ensure_type(dir, de);
+        if (de->d_type != DT_DIR)
+                return false;
+
+        *priority = prio;
+        return true;
+}
+
 enum {
         NO_TARGET_FOUND,
         TARGET_FOUND,
+        TARGET_NEEDS_CLEANUP,
 };
 
 /* find device node of device with highest priority */
@@ -169,6 +203,11 @@ static int link_find_prioritized(struct udev_device *dev, bool add, int dfd,
                         break;
                 if (dot_or_dot_dot(name))
                         continue;
+
+                if (!is_prio_dirent(dir, de, &prio)) {
+                        ret = TARGET_NEEDS_CLEANUP;
+                        break;
+                }
 
                 if (prio > priority) {
                         int priofd;
@@ -248,6 +287,8 @@ static int delete_target_entry(int dirfd, const char *prioname,
 {
         int priofd;
         int ret = 0, r;
+
+        unlinkat(dirfd, filename, 0);
 
         priofd = openat(dirfd, prioname, O_RDONLY|O_DIRECTORY);
         if (priofd == -1) {
@@ -360,6 +401,74 @@ static int _slink_semop(int semid, unsigned short semidx,
 #define unlock_slink(semid, semidx) \
         _slink_semop((semid), (semidx), 1, "release")
 
+static int cleanup_filter(const struct dirent *de)
+{
+        /*
+         * can't use  is_prio_dirent() here, because it needs to call
+         * dirent_ensure_type()
+         */
+        return !dot_or_dot_dot(de->d_name);
+}
+
+static void cleanup_old_targets(const char *dirname, struct udev_device *dev)
+{
+        struct udev *udev = udev_device_get_udev(dev);
+        struct dirent **darr;
+        int n;
+        int dfd = -1;
+        DIR *dir;
+
+        log_info("migrating symlink targets in %s", dirname);
+
+        /* Use scandir here to avoid races with deleting entries */
+        n = scandir(dirname, &darr, cleanup_filter, alphasort);
+        if (n < 0) {
+                log_error_errno(-errno, "error scanning %s", dirname);
+                return;
+        }
+        if (n == 0)
+                return;
+
+        dir = opendir(dirname);
+        if (dir != NULL)
+                dfd = dirfd(dir);
+        if (dfd == -1) {
+                log_error_errno(-errno, "error opening %s", dirname);
+                return;
+        }
+
+        while (n--) {
+                struct dirent *de = darr[n];
+                struct udev_device *ud;
+                int prio, r;
+                char prioname[PRIONAME_SIZE];
+
+                if (is_prio_dirent(dir, de, &prio))
+                        continue;
+                /* is_prio_dirent() called dirent_ensure_type() */
+                r = unlinkat(dfd, de->d_name,
+                             de->d_type == DT_DIR ? AT_REMOVEDIR : 0);
+                if (r == 0)
+                        log_debug("removed %s/%s", dirname, de->d_name);
+                else
+                        log_error_errno(-errno, "failed to remove %s/%s",
+                                        dirname, de->d_name);
+
+                ud = udev_device_new_from_device_id(udev, de->d_name);
+                if (ud == NULL)
+                        continue;
+
+                prio = udev_device_get_devlink_priority(ud);
+                udev_device_unref(ud);
+
+                make_prio_name(prio, prioname, sizeof(prioname));
+                create_target_entry(dfd, prioname, de->d_name, dirname);
+        }
+
+        closedir(dir);
+        free(darr);
+}
+
 /* manage "stack of names" with possibly specified device priorities */
 static void link_update(struct udev_device *dev, const char *slink, bool add) {
         char name_enc[UTIL_PATH_SIZE];
@@ -423,6 +532,13 @@ static void link_update(struct udev_device *dev, const char *slink, bool add) {
 
         r = link_find_prioritized(dev, add, dfd, dir,
                                   buf, sizeof(buf), slink);
+        if (r == TARGET_NEEDS_CLEANUP) {
+                cleanup_old_targets(dirname, dev);
+                r = link_find_prioritized(dev, add, dfd, dir,
+                                          buf, sizeof(buf), slink);
+                /* A single cleanup must be enough */
+                assert(r != TARGET_NEEDS_CLEANUP);
+        }
         if (r == NO_TARGET_FOUND) {
                 log_debug("no reference left, remove '%s'", slink);
                 if (unlink(slink) == 0)

mwilck/systemd-1

src/udev/udev-node.c
@@ -153,6 +153,7 @@ static bool is_prio_dirent(DIR *dir, struct dirent *de, int *priority)
 enum {
         NO_TARGET_FOUND,
         TARGET_FOUND,
+        TARGET_NEEDS_CLEANUP,
 };
 
 /* find device node of device with highest priority */
@@ -193,7 +194,7 @@ static int link_find_prioritized(sd_device *dev, bool add, DIR *dir,
                 if (dot_or_dot_dot(dent->d_name))
                         continue;
                 if (!is_prio_dirent(dir, dent, &prio))
-                        continue;
+                        return TARGET_NEEDS_CLEANUP;
 
                 if (prio > priority) {
                         _cleanup_closedir_ DIR *pdir = NULL;
@@ -272,6 +273,9 @@ static int delete_target_entry(int dirfd, const char *prioname,
 {
         _cleanup_close_ int priofd;
         int r;
+
+        /* Unlink legacy name, if present */
+        unlinkat(dirfd, filename, 0);
 
         priofd = openat(dirfd, prioname, O_RDONLY|O_DIRECTORY);
         if (priofd == -1) {
@@ -377,6 +381,69 @@ static int _slink_semop(int semid, unsigned short semidx,
 #define unlock_slink(semid, semidx) \
         _slink_semop((semid), (semidx), 1, "release")
 
+static int cleanup_filter(const struct dirent *de)
+{
+        /*
+         * can't use  is_prio_dirent() here, because it needs to call
+         * dirent_ensure_type()
+         */
+        return !dot_or_dot_dot(de->d_name);
+}
+
+static void cleanup_old_targets(const char *dirname, struct sd_device *dev)
+{
+        _cleanup_free_ struct dirent **darr = NULL;
+        _cleanup_closedir_ DIR *dir;
+        int dfd = -1, n;
+
+        log_info("migrating symlink targets in %s", dirname);
+
+        /* Use scandir here to avoid races with deleting entries */
+        n = scandir(dirname, &darr, cleanup_filter, alphasort);
+        if (n < 0) {
+                log_error_errno(-errno, "error scanning %s", dirname);
+                return;
+        }
+        if (n == 0)
+                return;
+
+        dir = opendir(dirname);
+        if (dir != NULL)
+                dfd = dirfd(dir);
+        if (dfd == -1) {
+                log_error_errno(-errno, "error opening %s", dirname);
+                return;
+        }
+
+        while (n--) {
+                _cleanup_(sd_device_unrefp) sd_device *ud = NULL;
+                _cleanup_free_ struct dirent *de = darr[n];
+                int prio, r;
+                char prioname[PRIONAME_SIZE];
+
+                if (is_prio_dirent(dir, de, &prio))
+                        continue;
+                /* is_prio_dirent() called dirent_ensure_type() */
+                r = unlinkat(dfd, de->d_name,
+                             de->d_type == DT_DIR ? AT_REMOVEDIR : 0);
+                if (r == 0)
+                        log_debug("removed %s/%s", dirname, de->d_name);
+                else
+                        log_error_errno(-errno, "failed to remove %s/%s",
+                                        dirname, de->d_name);
+
+                /* Now create the new-style entry */
+                if (sd_device_new_from_device_id(&ud, de->d_name) < 0)
+                        continue;
+
+                if (device_get_devlink_priority(ud, &prio) < 0)
+                        continue;
+
+                make_prio_name(prio, prioname, sizeof(prioname));
+                create_target_entry(dfd, prioname, de->d_name, dirname);
+        }
+}
+
 /* manage "stack of names" with possibly specified device priorities */
 static int link_update(sd_device *dev, const char *slink, bool add) {
         _cleanup_free_ char *target = NULL, *dirname = NULL;
@@ -448,6 +515,12 @@ static int link_update(sd_device *dev, const char *slink, bool add) {
                 return r;
 
         r = link_find_prioritized(dev, add, dir, slink, &target);
+        if (r == TARGET_NEEDS_CLEANUP) {
+                cleanup_old_targets(dirname, dev);
+                r = link_find_prioritized(dev, add, dir, slink, &target);
+                /* A single cleanup must be enough */
+                assert(r != TARGET_NEEDS_CLEANUP);
+        }
         if (r != TARGET_FOUND) {
                 log_debug("no reference left, remove '%s'", slink);
                 if (unlink(slink) == 0)